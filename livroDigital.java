3057739a2d2440d1957effa88d55d8c7da4ce867
RosaEmanoel <emanoelrosa0902@gmail.com>
2025-09-23 17:20:39 -0300

    Update livroDigital.java

--- livroDigital.java
+++ livroDigital.java
@@ -3,13 +3,14 @@ public class livroDigital extends livro {
         super(titulo, autor, anoPublicacao, numeroPaginas);
     }
 
-    @Override
+
     public String getFormato() {
         return "Digital";
     }
 
-    @Override
+
     public String toString() {
         return "[Digital] " + super.toString();
     }
 }
+
